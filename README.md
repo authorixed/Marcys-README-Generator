369d6e1f187f24108551bcb232f3c48f50348377
Josh Levy <joshlevycatches@gmail.com>
2024-08-12 19:29:24 -0400

    Add all JS and node

---
+++ README.md
@@ -0,0 +1,34 @@
+# Marcys-readMe-Gen
+
+
+
+## Description
+This project prompts the user with many questions to create their own readMe easily!
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+In order to use this project, you would need to clone the repository to your local machine. After that, open bash and route to that directory. Assuming you have node.js installed, you can launch the application with "node index.js", and you will recieve your prompts.
+
+## Usage
+Create readMe files easily!
+
+## License
+
+
+## Contributing
+n/a
+
+## Tests
+n/a
+
+## Questions
+If you have any questions, please feel free to contact me via:
+- GitHub: [authorixed](https://github.com/authorixed)
+- Email: joshlevycatches@gmail.com
---
+++ index.js
@@ -0,0 +1,67 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage information?',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'What are the contribution guidelines?',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'What are the test instructions?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPLv2', 'GPLv3', 'Apache', 'None'],
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+  },
+];
+
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('README.md generated!')
+  );
+}
+
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    const markdown = generateMarkdown(responses);
+    writeToFile('README.md', markdown);
+  });
+}
+
+init();
